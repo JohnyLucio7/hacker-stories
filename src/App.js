8d4691a30db18653d3e50b02baa26f1428c92e18
Johny Lucio <johnylucio7@gmail.com>
2024-10-24 12:21:15 -0300

    [Fundamentals of React] React Custom Hooks (Advanced)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import React from 'react';
 
+
+const useSemiPersistentState = (key, initialState) => {
+
+  let [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+  React.useEffect(() => { localStorage.setItem(key, value); }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () => {
 
   const stories = [
@@ -22,9 +32,7 @@ const App = () => {
     }
   ];
 
-  let [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
-
-  React.useEffect(() => { localStorage.setItem('search', searchTerm); }, [searchTerm]);
+  let [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
