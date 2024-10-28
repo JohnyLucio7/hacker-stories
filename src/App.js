5526e8baad8a35130fb1b5642d0594d50d98ccc8
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2024-10-28 10:19:04 -0300

    [Fundamentals of React] Forms in React

--- src/App.js
+++ src/App.js
@@ -99,33 +99,22 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
-    setUrl(`${API_ENDPOINT}${searchTerm}`)
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
   }
 
   return (
     <div>
 
-      <h1>
-        My Hacker Stories!
-      </h1>
+      <h1>My Hacker Stories!</h1>
 
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button
-        type='button'
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
       {stories.isError && <p>Something went wrong...</p>}
@@ -133,6 +122,27 @@ const App = () => {
     </div>
   );
 }
+
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button
+      type='button'
+      disabled={!searchTerm}
+    >
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
 
@@ -170,8 +180,7 @@ const List = ({ list, onRemoveItem }) => (
   </ul>
 );
 
-const Item = ({ item, onRemoveItem }) =>
-(
+const Item = ({ item, onRemoveItem }) => (
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
