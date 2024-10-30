375c94c9e49193953e9daefae7db9fa5260c6bc7
Johny Lucio <johnylucio7@gmail.com>
2024-10-30 19:51:01 -0300

    [React Maintenance - Testing in React] Test Suites, Test Cases, and Assertions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+describe('something truthy and falsy', () => {
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it('true to be true', () => {
+    expect(true).toBeTruthy()
+  });
+
+  it('false to be false', () => {
+    expect(false).toBeFalsy();
+  });
+
 });
