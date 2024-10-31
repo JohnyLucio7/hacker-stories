d5ac05f51d41fab8202579be649554a89633b28e
Johny Lucio <johnylucio7@gmail.com>
2024-10-31 19:52:21 -0300

    [React Maintenance - Testing in React] Integration Testing: Component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { storiesReducer, Item, SearchForm } from './App';
+import App, { storiesReducer, Item, SearchForm } from './App';
 import { render, screen, fireEvent, act, } from '@testing-library/react';
+import axios from 'axios';
+
+jest.mock('axios');
 
 // Exemplos de histórias
 const storyOne = {
@@ -103,5 +106,143 @@ describe('SearchForm', () => {
 
     expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
   });
+
+});
+
+describe('App', () => {
+  test('succeeds fetching data', async () => {
+    const promise = Promise.resolve({
+      data: {
+        hits: stories,
+      },
+    });
+    axios.get.mockImplementationOnce(() => promise);
+    render(<App />);
+    expect(screen.queryByText(/Loading/)).toBeInTheDocument();
+    await act(() => promise);
+    expect(screen.queryByText(/Loading/)).toBeNull();
+
+    expect(screen.getByText('React')).toBeInTheDocument();
+    expect(screen.getByText('Redux')).toBeInTheDocument();
+    expect(screen.getAllByText('Dismiss').length).toBe(2);
+  });
+
+  test('fails fetching data', async () => {
+
+    const promise = Promise.reject();
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    await act(async () => { render(<App />); });
+
+    try {
+      expect(screen.getByText(/Loading/)).toBeInTheDocument();
+      await act(async () => { await promise; });
+    } catch (error) {
+      expect(screen.queryByText(/Loading/)).toBeNull();
+      expect(screen.queryByText(/went wrong/)).toBeInTheDocument();
+    }
+
+  });
+
+  test('removes a story', async () => {
+
+    const promise = Promise.resolve({
+      data: {
+        hits: stories,
+      },
+    });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    render(<App />);
+
+    await act(() => promise);
+
+    expect(screen.getAllByText('Dismiss').length).toBe(2);
+
+    expect(screen.getByText('Jordan Walke')).toBeInTheDocument();
+
+    fireEvent.click(screen.getAllByText('Dismiss')[0]);
+
+    expect(screen.getAllByText('Dismiss').length).toBe(1);
+
+    expect(screen.queryByText('Jordan Walke')).toBeNull();
+
+  });
+
+  test('searches for specific stories', async () => {
+    const reactPromise = Promise.resolve({
+      data: {
+        hits: stories,
+      },
+    });
+
+    const anotherStory = {
+      title: 'JavaScript',
+      url: 'https://en.wikipedia.org/wiki/JavaScript',
+      author: 'Brendan Eich',
+      num_comments: 15,
+      points: 10,
+      objectID: 3,
+    };
+
+    const javascriptPromise = Promise.resolve({
+      data: {
+        hits: [anotherStory],
+      },
+    });
+
+    axios.get.mockImplementation((url) => {
+      if (url.includes('React')) {
+        return reactPromise;
+      }
+
+      if (url.includes('JavaScript')) {
+        return javascriptPromise;
+      }
+
+      throw Error();
+    });
+
+    // Initial render
+
+    render(<App />);
+
+    // First Data Fetching
+
+    await act(() => reactPromise);
+
+    expect(screen.queryByDisplayValue('React')).toBeInTheDocument();
+    expect(screen.queryByDisplayValue('JavaScript')).toBeNull();
+    expect(screen.queryByText('Jordan Walke')).toBeInTheDocument();
+    expect(screen.queryByText('Dan Abramov, Andrew Clark')).toBeInTheDocument();
+    expect(screen.queryByText('Brendan Eich')).toBeNull();
+
+    // Use interaction -> Search
+
+    fireEvent.change(screen.queryByDisplayValue('React'), {
+      target: {
+        value: 'JavaScript',
+      },
+    });
+
+    expect(screen.queryByDisplayValue('React')).toBeNull();
+    expect(
+      screen.queryByDisplayValue('JavaScript')
+    ).toBeInTheDocument();
+
+    fireEvent.submit(screen.queryByText('Submit'));
+
+    // Second Data Fetching
+    await act(() => javascriptPromise);
+
+    expect(screen.queryByText('Jordan Walke')).toBeNull();
+    expect(screen.queryByText('Dan Abramov, Andrew Clark')).toBeNull();
+    expect(screen.queryByText('Brendan Eich')).toBeInTheDocument();
+
+  });
+
+
 
 });
